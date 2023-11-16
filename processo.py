85b29c9fc45868ada2faef5fa80eaf9423db5b60
MnFCosta <mnfcosta1@gmail.com>
2023-11-16 11:53:39 -0300

    Testando threading para multiplas conexoes

--- processo.py
+++ processo.py
@@ -2,15 +2,37 @@ import socket
 import threading
 import time
 
+ultimo = 0
+semaphore = threading.Semaphore()
+
+# Endereço dos servidores no anel
+enderecos_servers = [
+    ('localhost', 8001),
+    ('localhost', 8002),
+    
+]
+
+""" def handle_client(conn, id):
+    # Recebe a mensagem enviada
+    data = conn.recv(1024)
+
+    data = data.decode('utf-8')
+    print(f"Server {id} recebeu: {data}")
+
+    conn.sendall('Olá!'.encode()) """
+
+
 def server(id, endereco, endereco_prox_servidor):
+    global ultimo
+    global enderecos_servers
     # Cria socket de servidor
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(endereco)
-    server_socket.listen(1)
-    participante = False
     lider = False
-
-    while True:
+    participante = False
+    server_socket.listen()
+    
+    """ def eleicao():
         print(f"Server {id} em {endereco} esperando conexão...")
         conn, endereco_cliente = server_socket.accept()
         print(f"Conexão de {endereco_cliente}")
@@ -27,29 +49,55 @@ def server(id, endereco, endereco_prox_servidor):
             print(data)
             data = f'{id}'
             print(data)
-            # Forward the message to the next server in the ring
+            # Manda mensagem para o próximo server no anel
             mandar_mensagem(endereco_prox_servidor, data.encode())
         else:
             if int(data) == id:
+                print(f"Processo {id} eleito o novo líder!")
                 lider = True
-                print(f"Processo {id} é o líder {lider}!")
 
             if int(data) > id:
                 participante = True
-                # Forward the message to the next server in the ring
+                # Manda mensagem para o próximo server no anel
                 mandar_mensagem(endereco_prox_servidor, data.encode())
             
             if int(data) < id and participante == False:
                 data = f'{id}'
+                # Manda mensagem para o próximo server no anel
                 mandar_mensagem(endereco_prox_servidor, data.encode())
             
         
         time.sleep(5)
 
-        
-
         # Fecha a conexão atual
-        conn.close()
+        conn.close() """
+
+    while True:
+        if id == ultimo:
+            lider = True
+
+        if lider:
+            print (f"Processo {id} é o lider, esperando conexões...")
+            conn, client_address = server_socket.accept()
+            print(f"Conexão de: {client_address}")
+
+            
+
+        if not lider:
+            lider = enderecos_servers[len(enderecos_servers) - 1]
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect(lider)
+
+            client_socket.sendall(f'Processo {id}: Olá lider!'.encode())
+
+
+            # Recebe a mensagem enviada
+            data = client_socket.recv(1024)
+
+            data = data.decode('utf-8')
+            print(f"Server {id} recebeu: {data}")
+            
+
 
 def mandar_mensagem(endereco_destino, mensagem):
     # Cria um socket com uma conexão ao próximo server no anel
@@ -60,8 +108,10 @@ def mandar_mensagem(endereco_destino, mensagem):
         client_socket.sendall(mensagem)
 
 def main():
-    # Endereço dos servidores no anel
-    enderecos_servers = [('localhost', 5000), ('localhost', 5001), ('localhost', 5002), ('localhost', 5003), ('localhost', 5004), ('localhost', 5005), ('localhost', 5006)]
+    global ultimo
+    global enderecos_servers
+    
+    ultimo = len(enderecos_servers)
 
     # Inicia uma thread pra cada server
     threads = []
@@ -71,9 +121,6 @@ def main():
         threads.append(thread)
         thread.start()
 
-    # Manda mensagem de eleicao pra iniciar o processo
-    mensagem_eleicao = "ELEICAO"
-    mandar_mensagem(enderecos_servers[0], mensagem_eleicao.encode())
 
     # espera todas as threads terminarem
     for thread in threads:
