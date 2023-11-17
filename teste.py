a32d21e3b155fbb07d3ebbb3539cbbddbb1decc4
MnFCosta <mnfcosta1@gmail.com>
2023-11-17 16:55:12 -0300

    Anel de conexão adicionado

--- teste.py
+++ teste.py
@@ -9,18 +9,19 @@ enderecos_servers = [
     ('localhost', 8003),   
 ]
 
-def cliente(id, endereco_prox_servidor,):
+def cliente(id, coordenador,):
+    print(coordenador)
     esperando_conexao = True
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while esperando_conexao:
         try:
-            client_socket.connect(endereco_prox_servidor)
+            client_socket.connect(coordenador)
             break
         except:
             pass
     
 
-    print(f"CONECTADO AO PROCESSO {endereco_prox_servidor}")
+    print(f"CONECTADO AO COORDENADOR {coordenador}")
 
     while True:
         client_socket.sendall(f'{id}'.encode())
@@ -29,9 +30,34 @@ def cliente(id, endereco_prox_servidor,):
         print(data)
         time.sleep(5)
 
+def mandar_msg(endereco_prox_servidor, mensagem):
+    print("Endereco")
+    esperando_conexao = True
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while esperando_conexao:
+        try:
+            client_socket.connect(endereco_prox_servidor)
+            break
+        except:
+            pass
+    client_socket.sendall(f'{mensagem}'.encode())
 
+    client_socket.close()
 
-            
+def eleicao(endereco):
+    print("ENTROU")
+    esperando_conexao = True
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while esperando_conexao:
+        try:
+            client_socket.connect(endereco)
+            break
+        except:
+            pass
+    client_socket.sendall(f'ELEICAO'.encode())
+
+    client_socket.close()
+        
 def server(id):
     global enderecos_servers
     coordenador = False
@@ -47,8 +73,7 @@ def server(id):
 
     print(f'Processo {id} com o endereco {endereco} escutando')
 
-    cliente_thread = threading.Thread(target=cliente, args=(id, endereco_prox_servidor,))
-    cliente_thread.start()
+    eleicao(endereco_prox_servidor)
 
     while True:
         conn, client_address = server_socket.accept()
@@ -57,17 +82,39 @@ def server(id):
         data = conn.recv(1024)
         data = data.decode('utf-8')
 
+        if data.split("|")[0] == 'ELEITO' and coordenador:
+            pass
+
+        if data.split("|")[0] == 'ELEITO' and not coordenador:
+            cliente_thread = threading.Thread(target=cliente, args=(data.split("|")[1], id))
+            cliente_thread.start()
+
         if data == 'Ping' and coordenador:
             print(f"Coordenador recebeu: {data}")
             conn.sendall('PONG'.encode())
         if data == 'ELEICAO':
-            print("ELEICAO")
+            msg = threading.Thread(target=mandar_msg, args=(endereco_prox_servidor, id))
+            msg.start()
         else:
-            print(data)
+            if coordenador:
+                pass
+            if int(data) == id:
+                print("Você é o novo coordenador!")
+                coordenador = True
+                msg = threading.Thread(target=mandar_msg, args=(endereco_prox_servidor, f'ELEITO|{id}'))
+                msg.start()
+
+            if int(data) < id:
+                print("passe pro proximo sua id")
+                msg = threading.Thread(target=mandar_msg, args=(endereco_prox_servidor, id))
+                msg.start()
+            if int(data) > id:
+                print("passe a id recebida para o proximo")
+                msg = threading.Thread(target=mandar_msg, args=(endereco_prox_servidor, int(data)))
+                msg.start()
 
         if not data:
             break
-        
 
 
 
